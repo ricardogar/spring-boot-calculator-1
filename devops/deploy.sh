c9565d8517eae906adff5bd6c43f44f6c99648dd
ricardogar <librafisi@gmail.com>
2020-02-01 17:07:43 -0500

    Update deploy.sh

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -1,7 +1,6 @@
 export DOCKER_HOST=tcp://127.0.0.1:4243
 export hostExposePort=$1
 echo "hostExposePort: ${hostExposePort}"
-docker rm -f $(docker ps -a -q --filter "name=calculator_instance${hostExposePort}")
 
 docker build -f src/main/docker/Dockerfile -t calculator:1.0 .
 docker run --rm -d -p ${hostExposePort}:8082 --name calculator_instance${hostExposePort} calculator:1.0
